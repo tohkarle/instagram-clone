1c74e965216156990f40209daf830ff949ce55b8
Toh Kar Le <tohkarle@suna-Macbook.local>
2023-06-28 14:48:30 +0800

    first commit

---
+++ instagramClone/App/instagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramCloneApp.swift
+//  instagramClone
+//
+//  Created by Toh Kar Le on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct instagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ instagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabView.swift
+//  instagramClone
+//
+//  Created by Toh Kar Le on 28/6/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
